9a5ac9a06d1c3cb263d157887f5d595fb6e303ac
jaeeunbaik <jaeeunbaik53@gmail.com>
2025-10-16 15:22:10 +0900

    refactor: test스크립트 변경

--- egs/librispeech/ASR/decode_0.sh
+++ egs/librispeech/ASR/decode_0.sh
@@ -24,5 +24,5 @@ CUDA_VISIBLE_DEVICES=0 python ./conformer_ctc_sd/decode.py \
     --max-duration 10 \
     --epoch 9 \
     --avg 4 \
-    --exp-dir conformer_ctc_sd/train70000-epoch77-avg10/exp_mse_20:1_single/models
+    --exp-dir conformer_ctc_sd/train70000-epoch77-avg10/exp_cs_20:1_single/models
 
